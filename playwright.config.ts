1a858c16e99c284dd63a06cdfdf9eeb848c37935
Xiolin Ramirez <80080294+XiolinRamirez@users.noreply.github.com>
2025-08-25 21:50:34 -0400

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from '@playwright/test';
+
 export default defineConfig({
+  testDir: 'e2e',
+  testMatch: /.*\.spec\.(js|ts)/,
   reporter: [['list'], ['junit', { outputFile: 'reports/junit/junit-e2e.xml' }]],
   use: { headless: true }
 });
